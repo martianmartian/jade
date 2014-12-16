c2736036410d5bf761d62202c563e119a1b9e920
iamwave007 <iamwave007@hotmail.com>
2014-12-16 18:51:45 -0500

    jade http://naltatis.github.io/jade-syntax-docs/

--- server.js
+++ server.js
@@ -1,11 +1,50 @@
 var express=require('express');
+var jade = require('jade');
 var app = express();
-//set path to the views (template) directory
-//app.set('view engine','jade')
+
 app.set('views', __dirname + '/views');
-//set path to static files
+
 app.use(express.static(__dirname + '/../public'));
-//handle GET requests on /
-app.get('/', function(req, res){res.render('index.jade', {title: 'Franz Enzenhofer'});});
-//listen on localhost:3000
+
+app.get('/', function(req, res){
+  res.render('index.jade', 
+    {name:"James",
+    book: {"name": "Hello", "price": 12.99},
+    input: {type:'text',name:'Bob'}
+    }
+    );
+});
 app.listen(3000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var express=require('express');
+// var jade = require('jade');
+// var app = express();
+// //set path to the views (template) directory
+// //app.set('view engine','jade')
+// app.set('views', __dirname + '/views');
+// //set path to static files
+// app.use(express.static(__dirname + '/../public'));
+// //handle GET requests on /
+// app.get('/', function(req, res){res.render('index.jade', {title: 'Franz Enzenhofer'});});
+// //listen on localhost:3000
+// app.listen(3000);
